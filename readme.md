89b6a52510a824ad38ea064521471fca760be91c
Toinane <toinane@yahoo.fr>
2017-04-10 18:03:49 +0200

    version 0.1.0

---
+++ readme.md
@@ -0,0 +1,60 @@
+# winston-istrace
+
+```
+npm i -S winston-istrace
+```
+
+## Configuration
+
+You need to pass host and service's name, the others params are optional.
+Others params will be global informations.
+
+- level param : this param from winston allow you to send only level of logs :
+by default, it send only error, warn and info logs.
+
+```json
+{
+  "host": "localhost:6031/log",
+  "service": "my service name",
+  "level": "debug",
+  "message": "my generic message",
+  "action": "my global action",
+  "scope": "my global scope",
+  "details": "if you have some globals objects"
+}
+```
+
+## Example
+
+```javascript
+const logger = require('winston');
+const IsTraceLogger = require('winston-istrace');
+
+logger.configure({
+  transports: [
+    new IsTraceLogger({
+      host: "http://requestb.in/1bx09nv1",
+      service: "my service name"
+    })
+  ]
+});
+
+logger.debug("Stream initialized");
+
+logger.info("Create new stream", {
+  details: {
+    name: "MyStream",
+    timestamp: new Date()
+  }
+});
+
+logger.warn("Stream in batch", {
+  code: 400
+});
+
+logger.error("Error on main stream", {
+  code: 300,
+  action: "get stream",
+  scope: "stream"
+});
+```
---
+++ src/transport.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var util = require('util');
+var Transport = require('winston').Transport;
+var request = require('request');
+
+function IsTraceLogger (options) {
+  this.name             = 'isTraceLogger';
+  this.payload          = {};
+  this.options          = options               || {};
+  this.level            = this.options.level    || 'info';
+  this.host             = this.options.host     || 'localhost';
+  this.payload.service  = this.options.service  || null;
+  this.payload.message  = this.options.message  || null;
+  this.payload.action   = this.options.action   || null;
+  this.payload.scope    = this.options.scope    || null;
+  this.payload.details  = this.options.details  || null;
+
+  Transport.call(this, options);
+}
+
+util.inherits(IsTraceLogger, Transport);
+
+IsTraceLogger.prototype.log = function (level, msg, meta, callback) {
+  var params = ['service', 'code', 'action', 'scope', 'details'];
+  var options = {
+    url: this.host,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    json: {}
+  };
+
+  for (var param of Object.keys(this.payload)) {
+    if (this.payload[param]) {
+      options.json[param] = this.payload[param];
+    }
+  }
+
+  for (var param of params) {
+    if(meta[param]){
+      options.json[param] = meta[param];
+    }
+  }
+
+  if (!options.json.code) {
+    switch (level) {
+      case 'error': options.json.code = 520; break;
+      case 'warn': options.json.code = 202; break;
+      default: options.json.code = 200;
+    }
+  }
+
+  options.json.message = msg;
+
+  request(options, function (err, res, body) {
+    if (body && (body.code === 200)) {
+      callback(null, true);
+    } else {
+      callback(body, null);
+    }
+  });
+}
+
+module.exports = IsTraceLogger;
