c3875e37b4752f201c875bdbf3de7bfac9cc0c05
Toinane <toinane@crea-that.fr>
2019-08-06 21:45:38 +0200

    update projet & dependencies

--- src/transport.js
+++ src/transport.js
@@ -1,19 +1,19 @@
-'use strict';
+const util = require('util');
+const Transport = require('winston').Transport;
+const request = require('request');
 
-var util = require('util');
-var Transport = require('winston').Transport;
-var request = require('request');
+const CODE_200 = 200;
 
 function IsTraceLogger (options) {
-  this.name             = 'isTraceLogger';
-  this.payload          = {};
-  this.options          = options               || {};
-  this.host             = this.options.host     || 'localhost';
-  this.payload.service  = this.options.service  || null;
-  this.payload.message  = this.options.message  || null;
-  this.payload.action   = this.options.action   || null;
-  this.payload.scope    = this.options.scope    || null;
-  this.payload.details  = this.options.details  || null;
+  this.name = 'isTraceLogger';
+  this.payload = {};
+  this.options = options || {};
+  this.host = this.options.host || 'localhost';
+  this.payload.service = this.options.service || null;
+  this.payload.message = this.options.message || null;
+  this.payload.action = this.options.action || null;
+  this.payload.scope = this.options.scope || null;
+  this.payload.details = this.options.details || null;
 
   Transport.call(this, options);
 
@@ -23,8 +23,8 @@ function IsTraceLogger (options) {
 util.inherits(IsTraceLogger, Transport);
 
 IsTraceLogger.prototype.log = function (level, msg, meta, callback) {
-  var params = ['service', 'code', 'action', 'scope', 'details'];
-  var options = {
+  const params = ['service', 'code', 'action', 'scope', 'details'];
+  let options = {
     url: this.host,
     method: 'POST',
     headers: {
@@ -33,13 +33,13 @@ IsTraceLogger.prototype.log = function (level, msg, meta, callback) {
     json: {}
   };
 
-  for (var param of Object.keys(this.payload)) {
+  for (const param of Object.keys(this.payload)) {
     if (this.payload[param]) {
       options.json[param] = this.payload[param];
     }
   }
 
-  for (var param of params) {
+  for (const param of params) {
     if (typeof meta === Object){
       if (meta[param]){
         options.json[param] = meta[param];
@@ -49,21 +49,21 @@ IsTraceLogger.prototype.log = function (level, msg, meta, callback) {
 
   if (!options.json.code) {
     switch (level) {
-      case 'error': options.json.code = 520; break;
-      case 'warn': options.json.code = 202; break;
-      default: options.json.code = 200;
+    case 'error': options.json.code = 520; break;
+    case 'warn': options.json.code = 202; break;
+    default: options.json.code = 200;
     }
   }
 
   options.json.message = msg;
 
   request(options, function (err, res, body) {
-    if (body && (body.code === 200)) {
+    if (body && (body.code === CODE_200)) {
       callback(null, true);
     } else {
       callback(body, null);
     }
   });
-}
+};
 
 module.exports = IsTraceLogger;
